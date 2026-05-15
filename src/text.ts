adad75b785d70fc8abae8b4fbd850c45b5611a38
mao-ymst <47tegu8yun@gmail.com>
2026-05-15 14:35:32 +0900

    first commit TypeScript tutorial

---
+++ src/text.ts
@@ -0,0 +1,21 @@
+export const text = () => {
+    
+    //基本のdataの形
+    const name:string = "nyon";
+    const age:number = 5;
+    const isStudent:boolean = false;
+    const hobbies:string[] = ["Reading", "Coding", "Gaming"];
+    const address:object = {
+        street: "123 Main St",
+        city:"Los Angeles",
+        state: "CA"
+    };
+    const today:Date = new Date();
+    const none:null = null;
+    const notDefined:undefined = undefined;
+    const random:any = "Hello World";
+
+    const gender:"male" | "female" = "male";
+    const value:string | number | null = "Hello World";
+
+} 
