2176853601f2d4d4e36817bafdcf1e44a68b5fac
mao-ymst <47tegu8yun@gmail.com>
2026-05-15 15:36:19 +0900

    feat:implement reverseArray function

--- src/text.ts
+++ src/text.ts
@@ -18,6 +18,27 @@ export const text = () => {
     const gender:"male" | "female" = "male";
     const value:string | number | null = "Hello World";
 
-    
+    //問題：引数に文字列の配列を受け取り　その中身を順番を逆にして返す関数を作成する
+    //例：　reverseArray(["a","b","c"]) => ["c,"b","a"]
+    //実際にデータ型を定義してみる：変数、引数、返り値
 
+
+    //引数の隣：　の隣にstring[]を置くことで　これを返り値に指定することができる
+    const reverseArray = (arr: string[]):string[] => {
+
+        // return arr.reverse();　既存の順番を逆にできる機能
+
+        const reversedArray:string[] = []; //結果用の空っぽの新しい箱を用意する
+        let index:number = arr.length - 1; //元の箱の一番後ろの文字からスタートできるように一番後ろを確認　-1することでインデックス番号と帳尻を合わせる
+        while(index >= 0) {
+            reversedArray.push(arr[index]); //indexが０より大きいなら新しい箱に入れる　まず指定した一番後ろの数を入れて
+            index--; // indexの番号を１つずつ減らしていく　3->2->1みたいに
+        }
+        return reversedArray; //中身が新しくなったら配列を返す
+
+    }
+
+    console.log(reverseArray(["a","b","c"]));
 } 
+
+text();
